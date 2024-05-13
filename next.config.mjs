9aaee7e2675fa0601c683537a4ebbdef7a2f94ca
ARAV SHARMA <aravsharma.2008@gmail.com>
2024-05-13 16:35:38 -0500

    ghpages4

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,7 @@
-// next.config.js
+// next.config.mjs
 import withOptimizedImages from 'next-export-optimize-images';
 
 export default withOptimizedImages({
   output: 'export',
   trailingSlash: true,
-  exportPathMap: async function (
-    defaultPathMap,
-    { dev, dir, outDir, distDir, buildId }
-  ) {
-    return {
-      ...defaultPathMap,
-    };
-  },
 });
